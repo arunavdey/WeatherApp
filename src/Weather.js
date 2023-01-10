4b04d1e1840b887623e58f97403156c5fe9a6676
= <=>
2023-01-10 12:31:21 +0530

    format code

--- src/Weather.js
+++ src/Weather.js
@@ -18,39 +18,3 @@ async function getData(city) {
 }
 
 export default getData;
-
-// getData("Bengaluru");
-//getData("inikbcw");
-
-// Console Output:
-//
-// {
-//     coord: { lon: 77.6033, lat: 12.9762 },
-//     weather: [
-//       { id: 801, main: 'Clouds', description: 'few clouds', icon: '02n' }
-//     ],
-//     base: 'stations',
-//     main: {
-//       temp: 290.95,
-//       feels_like: 290.14,
-//       temp_min: 290.95,
-//       temp_max: 292.05,
-//       pressure: 1021,
-//       humidity: 52
-//     },
-//     visibility: 6000,
-//     wind: { speed: 1.54, deg: 110 },
-//     clouds: { all: 20 },
-//     dt: 1673277014,
-//     sys: {
-//       type: 1,
-//       id: 9205,
-//       country: 'IN',
-//       sunrise: 1673226845,
-//       sunset: 1673267902
-//     },
-//     timezone: 19800,
-//     id: 1277333,
-//     name: 'Bengaluru',
-//     cod: 200
-//   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,80 +1,75 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react'
+import * as React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
-import '../components/SearchBar.css'
-
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import "../components/SearchBar.css";
 
 export default function SearchBar(props) {
-
-  const [inputData, setInputData] = useState('');
+  const [inputData, setInputData] = useState("");
   const [city, setCity] = useState(getLocalItems);
   const navigate = useNavigate();
 
-function getLocalItems() {
-    let list = localStorage.getItem('lists');
+  function getLocalItems() {
+    let list = localStorage.getItem("lists");
     console.log(list);
 
     if (list) {
-        // console.log(JSON.parse(localStorage.getItem('lists')));
-        return JSON.parse(localStorage.getItem('lists'));
+      // console.log(JSON.parse(localStorage.getItem('lists')));
+      return JSON.parse(localStorage.getItem("lists"));
+    } else {
+      return [];
     }
-
-    else
-    {
-        return [];
-    }
-}
-
-
-
-function handleSearch() {
-  if(inputData)
-  {
-    localStorage.setItem('lists', JSON.stringify([...city, inputData]))
-    setCity([...city, inputData]);
-    // props.setCityName(inputData)
-    setInputData('');
-    navigate("/results");
-
   }
-}
 
-// useEffect(() => {
-//   localStorage.setItem('lists', JSON.stringify(city))
-// }, [city]);
+  function handleSearch() {
+    if (inputData) {
+      localStorage.setItem("lists", JSON.stringify([...city, inputData]));
+      setCity([...city, inputData]);
+      // props.setCityName(inputData)
+      setInputData("");
+      navigate("/results");
+    }
+  }
+
+  // useEffect(() => {
+  //   localStorage.setItem('lists', JSON.stringify(city))
+  // }, [city]);
 
   return (
-    <div className='search'>
-    <div className='search-input'>
-      <Autocomplete
-        freeSolo
-        id="free-solo-2-demo"
-        // value={inputData} 
-        // onChange={(e) => setInputData(e.target.value) }
-        style={{ width:"50%" }}
-        disableClearable
-        options={city.reverse()}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            value={inputData} 
-            onChange={(e) => {setInputData(e.target.value);props.setCityName(e.target.value) }}
-            label= 'Enter city name'
-            InputProps={{
-              ...params.InputProps,
-              type: 'search',
-            }}
-          />
-        )}
-      />
+    <div className="search">
+      <div className="search-input">
+        <Autocomplete
+          freeSolo
+          id="free-solo-2-demo"
+          // value={inputData}
+          // onChange={(e) => setInputData(e.target.value) }
+          style={{ width: "50%" }}
+          disableClearable
+          options={city.reverse()}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              value={inputData}
+              onChange={(e) => {
+                setInputData(e.target.value);
+                props.setCityName(e.target.value);
+              }}
+              label="Enter city name"
+              InputProps={{
+                ...params.InputProps,
+                type: "search",
+              }}
+            />
+          )}
+        />
 
-      <button className='search-button' onClick ={handleSearch}>Search</button>
+        <button className="search-button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
     </div>
-    </div>
-
   );
 }
