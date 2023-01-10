911a4d32fef213bf225678bd0ab81b913c0d0f08
ArunavDey <arunavdey01@gmail.com>
2023-01-10 17:20:39 +0530

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <footer>
-      {/* <p>This is a footer</p> */}
-    </footer>
-  );
+  return <footer>{<p>This is a footer</p> }</footer>;
 }
 
 export default Footer;
