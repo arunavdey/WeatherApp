e266395fb5292460a17f1019353dc9aae7077bee
Sethupathy <sethupathy@GS0312.local>
2023-01-10 11:10:57 +0530

    add styling and navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route, Link } from "react-router-dom";
 // import { Link } from 'react-router';
 import SearchBar from "./SearchBar"
 import ResultPage from "./ResultPage"
+import "../components/Header.css"
 
 function Header() {
   return (
@@ -10,17 +11,18 @@ function Header() {
     <header>
       <h1>This is the nav bar </h1>
     </header>
-
+    <div className="nav-tabs">
       <a href='/search'><h1>Search</h1></a>
-        <a href='/results'><h1>Result</h1></a>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/search" element={<SearchBar />}></Route>
-          <Route exact path="/results" element={<ResultPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <a href='/results'><h1>Result</h1></a>
     </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/search" element={<SearchBar />}></Route>
+            <Route exact path="/results" element={<ResultPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+  
   );
 }
 
