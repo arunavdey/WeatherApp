6e6ecf3eee6af6ea181d9c645ac19844faee14f6
= <=>
2023-01-10 11:57:24 +0530

    Move Weather.js to src directory

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,25 @@
 async function getData(city) {
-    apiKey = "aac0e38a5a3c567cd3e79a723bf39fc3";
+  const apiKey = "aac0e38a5a3c567cd3e79a723bf39fc3";
 
-    url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey;
 
-    try {
-        const result = await fetch(url);
-            result.json().then(json => {
-            console.log(json);
-            // return json;
-        })
-    }
-    catch (error)   {
-        console.log('There was an error', error);
-    }
+  try {
+    const result = await fetch(url);
+    result.json().then((json) => {
+      console.log(json);
+    });
+  } catch (error) {
+    console.log("There was an error:", error);
+  }
 }
 
-getData("Bengaluru");
+export default getData;
+
+// getData("Bengaluru");
 //getData("inikbcw");
 
 // Console Output:
